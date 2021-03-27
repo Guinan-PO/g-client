47309de5d196c45080fbb79a6d4983178693fd39
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2021-03-27 20:39:45 -0300

    feat: add LoginComponentRoute

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
+import { LoginModule } from './modules/login/login.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
