c1eb898f4616721ffb9628c5cc4fc112bcf6e980
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2021-03-28 00:43:18 -0300

    feat(iAuthHttpInterceptor): add initial structure

---
+++ src/app/core/interceptors/HttpAuth/http-auth.interceptor.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from '../../services/authentication/authentication.service';
+
+@Injectable()
+export class HttpAuthInterceptor implements HttpInterceptor {
+  constructor(private authenticationService: AuthenticationService) {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const user = this.authenticationService.user;
+
+    if (user?.token) {
+      request = request.clone({
+        setHeaders: { Authorization: user.token }
+      });
+    }
+
+    return next.handle(request);
+  }
+}
