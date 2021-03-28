1657069d63e2525c665fb07382a7a3cfda937c44
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2021-03-28 19:06:23 -0300

    refactor(erroInterceptor): add modal erro message

--- src/app/core/interceptors/HttpError/http-error.interceptor.ts
+++ src/app/core/interceptors/HttpError/http-error.interceptor.ts
@@ -1,17 +1,23 @@
+import { MatDialog } from '@angular/material/dialog';
 import { Injectable } from '@angular/core';
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { AuthenticationService } from '../../services/authentication/authentication.service';
-import { catchError } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
+import { ModalErrorComponent } from 'src/app/shared/components/modal-error/modal-error.component';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(
+    private authenticationService: AuthenticationService,
+    private dialog: MatDialog
+  ) {}
 
   intercept(
     request: HttpRequest<any>,
@@ -19,14 +25,27 @@ export class HttpErrorInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError((err) => {
-        if (err.status === 401) {
-          // auto logout if 401 response returned from api
-          this.authenticationService.logout();
-          location.reload();
+        if (err instanceof HttpErrorResponse) {
+          let errorMessage = err.error.errors.toString();
+
+          if (err.status === 401) {
+            this.authenticationService.logout();
+            errorMessage = 'Usuário não autenticado';
+          }
+
+          this.dialog.open(ModalErrorComponent, {
+            data: {
+              title: 'Houve um erro durante a chamada ao Servidor',
+              errorMessage
+            }
+          });
+
+          this.dialog.afterAllClosed.pipe(tap((_) => location.reload()));
+          return next.handle(request);
         }
 
-        const error = err.error.message || err.statusText;
-        return throwError(error);
+        console.error('An error occurred:', err.error.message);
+        return next.handle(request);
       })
     );
   }
