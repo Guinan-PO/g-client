19a32fb8173ce4c6cb57cc1be4f41b400a666d97
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2021-03-28 19:14:27 -0300

    build(sharedModule): add MatButton and MatDialog module

--- src/app/shared/shared-module.module.ts
+++ src/app/shared/shared-module.module.ts
@@ -1,13 +1,23 @@
+import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputTextComponent } from './components/input-fields/input-text/input-text.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { LogoComponent } from './components/logo/logo.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ModalErrorComponent } from './components/modal-error/modal-error.component';
 
 @NgModule({
-  declarations: [InputTextComponent, LogoComponent],
-  imports: [CommonModule, MatInputModule, ReactiveFormsModule, FormsModule],
+  declarations: [InputTextComponent, LogoComponent, ModalErrorComponent],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatDialogModule,
+    MatButtonModule
+  ],
   exports: [InputTextComponent, LogoComponent]
 })
 export class SharedModuleModule {}
