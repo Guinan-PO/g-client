0e1f2d6147b836878c98398b8361165ab313a1e4
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2021-03-28 00:44:18 -0300

    feat(authService): add initial structure

---
+++ src/app/core/services/authentication/authentication.service.ts
@@ -0,0 +1,53 @@
+import { ResponseAuth } from './../../../shared/Models/Responses/ResponseAuth';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from 'src/app/shared/Models/User';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  private userSource$: BehaviorSubject<User>;
+  public user$: Observable<User>;
+
+  public get user(): User {
+    return this.userSource$.value;
+  }
+
+  public set user(user: User) {
+    this.userSource$.next(user);
+  }
+
+  constructor(private httpClient: HttpClient) {
+    this.userSource$ = new BehaviorSubject<User>(
+      JSON.parse(localStorage.getItem('guinan-user'))
+    );
+    this.user$ = this.userSource$.asObservable();
+  }
+
+  public login(username: string, password: string): Observable<User> {
+    if (!username || !password) throw new Error('[login] no params');
+
+    return this.httpClient
+      .post<ResponseAuth>(`${environment.apiUrl}/tokens`, {
+        username,
+        password
+      })
+      .pipe(
+        map((response) => {
+          response.user.token = response.token;
+          localStorage.setItem('user', JSON.stringify(response));
+          this.userSource$.next(response.user);
+          return response.user;
+        })
+      );
+  }
+
+  public logout(): void {
+    localStorage.removeItem('user');
+    this.userSource$.next(null);
+  }
+}
