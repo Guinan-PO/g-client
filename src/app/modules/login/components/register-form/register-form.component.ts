140235750278358842f57b54264c6cfde11af297
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2021-03-27 22:36:12 -0300

    refactor(registerForm): add mutual form style.css and add name field

--- src/app/modules/login/components/register-form/register-form.component.ts
+++ src/app/modules/login/components/register-form/register-form.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/shared/Models/User';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.css']
+  styleUrls: ['../formsStyle.css', './register-form.component.css']
 })
 export class RegisterFormComponent {
+  @Output() onFormSubmitted: EventEmitter<User> = new EventEmitter();
+  @Output() onChangeToRegisterForm: EventEmitter<boolean> = new EventEmitter();
+
   public _registerForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [
       Validators.required,
@@ -15,7 +20,13 @@ export class RegisterFormComponent {
     ])
   });
 
-  constructor() {}
+  public submit(): void {
+    this._registerForm.markAllAsTouched();
+    if (this._registerForm.invalid) return;
+    this.onFormSubmitted.emit(this._registerForm.value);
+  }
 
-  public submit() {}
+  public setEditMode(state: boolean): void {
+    this.onChangeToRegisterForm.emit(state);
+  }
 }
