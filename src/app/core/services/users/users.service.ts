9034ebf84ee776124e4f5926856eea9b50d486a6
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2021-03-28 19:08:01 -0300

    feat(userService): add createUser post

--- src/app/core/services/users/users.service.ts
+++ src/app/core/services/users/users.service.ts
@@ -1,13 +1,32 @@
-import { Observable } from 'rxjs';
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from 'src/app/shared/Models/User';
+import { environment } from 'src/environments/environment';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
-  constructor(private httpClient: HttpClient) {}
+  private userSource$ = new BehaviorSubject<User>(null);
+  public user$ = new Observable<User>();
 
-  public createUser(user: User): Observable<HttpResponse<void>> | void {}
+  public get user(): User {
+    return this.userSource$.value;
+  }
+
+  public set user(user: User) {
+    this.userSource$.next(user);
+  }
+
+  constructor(private httpClient: HttpClient) {
+    this.user$ = this.userSource$.asObservable();
+  }
+
+  public createUser(user: User): Observable<User> {
+    return this.httpClient
+      .post<User>(`${environment.apiUrl}/users`, user)
+      .pipe(tap((user) => this.userSource$.next(user)));
+  }
 }
