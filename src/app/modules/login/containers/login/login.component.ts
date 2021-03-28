44de03cf347901e3020a69e73240b1f5e0dac338
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2021-03-28 19:09:37 -0300

    feat(loginContainer): add formRegisterSubmitted function

--- src/app/modules/login/containers/login/login.component.ts
+++ src/app/modules/login/containers/login/login.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { FormLogin } from 'src/app/shared/Models/FormLogin';
 import { User } from 'src/app/shared/Models/User';
+import { environment } from './../../../../../environments/environment';
+import { UsersService } from './../../../../core/services/users/users.service';
 
 @Component({
   selector: 'app-login',
@@ -9,16 +13,34 @@ import { User } from 'src/app/shared/Models/User';
 })
 export class LoginComponent implements OnInit {
   public editMode: boolean = false;
+  public _user$: Observable<User>;
 
-  constructor() {}
+  public userEmail: string;
+  public isLoading = false;
+
+  constructor(private userService: UsersService) {}
 
   ngOnInit(): void {}
+
+  public formLoginSubmitted(formObj: FormLogin) {}
+
+  public formRegisterSubmitted(user: User) {
+    this.isLoading = true;
+    this._user$ = this.userService.createUser(user).pipe(
+      tap((user) => {
+        this.userEmail = user.email;
+        this.isLoading = false;
+        this.editMode = false;
+      })
+    );
+  }
 
   public setEditMode(state: boolean) {
     this.editMode = state;
   }
 
-  public formLoginSubmitted(formObj: FormLogin) {}
-
-  public formRegisterSubmitted(user: User) {}
+  private setLocalStorage = (user: User): void => {
+    console.log('setLocalStorage chamado');
+    localStorage.setItem(environment.localStorage.user, JSON.stringify(user));
+  };
 }
