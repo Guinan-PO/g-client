4d6d09204285bfebb8bc489e421c51811df96462
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2021-03-29 02:53:34 -0300

    feat(boardList): add initial structure

---
+++ src/app/modules/boards/components/boards-list/boards-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boards-list',
+  templateUrl: './boards-list.component.html',
+  styleUrls: ['./boards-list.component.css']
+})
+export class BoardsListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
