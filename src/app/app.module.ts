51a0bd37a785906cfd47e773d3d6eced7088d3a5
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2021-03-29 00:50:40 -0300

    feat(appModule): add HttpAuthInterceptor as provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { HttpErrorInterceptor } from './core/interceptors/HttpError/http-error.interceptor';
-import { LoginModule } from './modules/login/login.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpAuthInterceptor } from './core/interceptors/HttpAuth/http-auth.interceptor';
+import { HttpErrorInterceptor } from './core/interceptors/HttpError/http-error.interceptor';
+import { LoginModule } from './modules/login/login.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,6 +19,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
     LoginModule
   ],
   providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: HttpAuthInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
