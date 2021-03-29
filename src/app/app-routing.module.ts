b7f6f45e51d9395557aaebf3ab9c11fb353a9ea3
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2021-03-29 02:58:53 -0300

    refactor(appRouting): change boards to root and set as lazyLoad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
-import { LoginComponent } from './modules/login/containers/login/login.component';
+import { AuthGuard } from './core/guards/auth/auth.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/login/containers/login/login.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'boards', pathMatch: 'full' },
   {
-    path: '',
+    path: 'boards',
+    loadChildren: () =>
+      import('src/app/modules/boards/board.module').then((m) => m.BoardModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'login',
     component: LoginComponent
-  }
+  },
+  { path: '**', redirectTo: 'boards', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { BoardModule } from './modules/boards/board.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
