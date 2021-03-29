4d85647aeca87c01c96fb5efaa0bedd745266dc4
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2021-03-29 00:49:48 -0300

    refactor(userService): remove unused code

--- src/app/core/services/users/users.service.ts
+++ src/app/core/services/users/users.service.ts
@@ -1,32 +1,16 @@
-import { Observable, BehaviorSubject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { User } from 'src/app/shared/Models/User';
 import { environment } from 'src/environments/environment';
-import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
-  private userSource$ = new BehaviorSubject<User>(null);
-  public user$ = new Observable<User>();
-
-  public get user(): User {
-    return this.userSource$.value;
-  }
-
-  public set user(user: User) {
-    this.userSource$.next(user);
-  }
-
-  constructor(private httpClient: HttpClient) {
-    this.user$ = this.userSource$.asObservable();
-  }
+  constructor(private httpClient: HttpClient) {}
 
   public createUser(user: User): Observable<User> {
-    return this.httpClient
-      .post<User>(`${environment.apiUrl}/users`, user)
-      .pipe(tap((user) => this.userSource$.next(user)));
+    return this.httpClient.post<User>(`${environment.apiUrl}/users`, user);
   }
 }
