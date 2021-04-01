36b35336f8e5c3d9f7c94f107aeb75cf1d08d011
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2021-04-01 18:43:38 -0300

    refactor: remove boardComponent to appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
-import { BoardModule } from './modules/boards/board.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpAuthInterceptor } from './core/interceptors/HttpAuth/http-auth.interceptor';
