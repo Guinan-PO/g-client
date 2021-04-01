8174999024f5da08db5070df7c5dfc0b70597288
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2021-04-01 18:39:02 -0300

    refactor(boardHomeContainer): change responsability to loadingService

--- src/app/modules/boards/containers/boards-home/boards-home.component.ts
+++ src/app/modules/boards/containers/boards-home/boards-home.component.ts
@@ -1,4 +1,8 @@
+import { CategoriesService } from '../../../../core/services/categories/categories.service';
+import { LoadingService } from './../../services/loading/loading.service';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import Category from 'src/app/shared/Models/Category';
 
 @Component({
   selector: 'app-boards-home',
@@ -6,9 +10,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./boards-home.component.css']
 })
 export class BoardsHomeComponent implements OnInit {
-  public isLoading: boolean = false;
+  public isLoading$: Observable<boolean>;
 
-  constructor() {}
+  constructor(private loadingService: LoadingService) {
+    this.isLoading$ = this.loadingService.isLoading$;
+  }
 
   ngOnInit(): void {}
 }
