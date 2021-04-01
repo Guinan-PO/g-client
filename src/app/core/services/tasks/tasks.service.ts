8d989d52a171eda016a0d1fa93a1aa8b3436d823
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2021-04-01 18:36:56 -0300

    feat(taskService): add initial structure

---
+++ src/app/core/services/tasks/tasks.service.ts
@@ -0,0 +1,38 @@
+import { Task } from 'src/app/shared/Models/Task';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TasksService {
+  constructor(private httpClient: HttpClient) {}
+
+  public getTasks(
+    situation_id: number,
+    page?: number,
+    pageSize?: number
+  ): Observable<Task[]> {
+    const optionsHttp = this.setPaginatorParams(situation_id, page, pageSize);
+
+    return this.httpClient.get<Task[]>(
+      `${environment.apiUrl}/tasks`,
+      optionsHttp
+    );
+  }
+
+  private setPaginatorParams(
+    situation_id: number,
+    page: number,
+    pageSize: number
+  ): { params?: any } {
+    let params = new HttpParams();
+    if (page) params = params.append('page', page.toString());
+    if (pageSize) params = params.append('pageSize', pageSize.toString());
+
+    params = params.append('situation_id', situation_id.toString());
+    return { params };
+  }
+}
