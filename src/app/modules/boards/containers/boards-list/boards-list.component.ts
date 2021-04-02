182ce5c904e39f320d83b5c5e1e5274843b83b05
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2021-04-02 17:01:18 -0300

    refactor(boardListContainer): refactor loadSituations observables

--- src/app/modules/boards/containers/boards-list/boards-list.component.ts
+++ src/app/modules/boards/containers/boards-list/boards-list.component.ts
@@ -4,10 +4,11 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map, mergeMap, tap } from 'rxjs/operators';
+import { concatMap, map, mergeMap, tap } from 'rxjs/operators';
 import { CategoriesService } from 'src/app/core/services/categories/categories.service';
 import Category from 'src/app/shared/Models/Category';
 import { Situation } from 'src/app/shared/Models/Situation';
+import { Task } from 'src/app/shared/Models/Task';
 
 @Component({
   selector: 'app-boards-list',
@@ -17,11 +18,14 @@ import { Situation } from 'src/app/shared/Models/Situation';
 export class BoardsListComponent implements OnInit {
   public taskSearchInput: FormControl = new FormControl('');
   public selectedTabIndex: number = 0;
+  public loadTasks: boolean = false;
 
   public _categories$ = this.categoriesService.categories$;
-  public __categories$ = new Observable();
+  public __categories$: Observable<Category[]> = new Observable();
+  public __tasks$: Observable<Task[]> = new Observable();
+  // public __situations$: Observable<Situation[]> = new Observable();
 
-  public _situations$: Observable<Observable<Situation[]>> = new Observable();
+  public category: Category;
 
   constructor(
     private router: Router,
@@ -31,68 +35,60 @@ export class BoardsListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.__categories$ = this.categoriesService
-      .getCategories(0, 15)
-      .pipe(tap((c) => console.log('carregou as categorias:', c)));
+    this.loadInitialCategories();
   }
 
-  public createBoardBtn() {
+  public createBoardBtn(): void {
     this.router.navigateByUrl('/boards/new');
   }
 
-  public loadSituations($event) {
-    console.log('loadSituations called. Event: ', $event);
+  public loadSituations({ index }): void {
+    console.log('loadSituations called. Event: ', index);
 
-    const category: Category = this.categoriesService.categories[$event.index];
+    this.category = this.categoriesService.categories[index];
 
-    if (!!category.situations$) {
+    if (!!this.category.situations$) {
       console.log('retornando pq ja existe');
       return;
     }
 
-    this._situations$ = this._categories$.pipe(
-      map((categories) => {
-        console.log('categories map teste', categories);
-        console.log('categories map teste', categories[$event.index]);
-
-        categories[
-          $event.index
-        ].situations$ = this.situationsService.getSituations(category.id).pipe(
-          tap((situations) => {
-            situations.map((s) => {
-              console.log('tasks', s);
-              s.tasks = this.taskService.getTasks(s.id, 0, 15);
-            });
-          })
-        );
-
-        return categories[$event.index].situations$;
+    this.category.situations$ = this.loadInitialSituations(
+      this.category.id
+    ).pipe(
+      tap((situations) => {
+        console.log('situations situations situations');
+        situations.map((s) => {
+          console.log('aaaaaaaaaaaaaaaaaa', s);
+          s.tasks = this.loadInitialTasks(s.id);
+        });
       })
+    );
+  }
 
-      // mergeMap((categories) => {
-      //   console.log('categories mergeMap', categories);
+  public loadMoreTask(situation_id: number): void {
+    this.taskService.page = +1;
 
-      //   categories.map((category) => {
-      //     console.log('category map', category);
+    this.__tasks$ = this.taskService.getTasks(situation_id);
 
-      //     if (category != categories[$event.index]) return;
+    this.loadTasks = true;
+  }
 
-      //     categories[
-      //       $event.index
-      //     ].situations$ = this.situationsService
-      //       .getSituations($event.index + 1)
-      //       .pipe(
-      //         // tap((sitautions) => console.log('situations:', sitautions)),
-      //         tap((situations) =>
-      //           situations.map((s) => {
-      //             s.tasks = this.taskService.getTasks(s.id, 0, 15);
-      //           })
-      //         )
-      //       );
-      //   });
-      //   return categories[$event.index].situations$;
-      // })
-      // console.log('chegou aqui no category2: ', categories[$event.index]);
+  private loadInitialTasks = (situation_id: number): Observable<Task[]> => {
+    return this.taskService.getTasks(situation_id);
+  };
+
+  private loadInitialSituations = (
+    category_id: number
+  ): Observable<Situation[]> => {
+    return this.situationsService.getSituations(category_id);
+  };
+
+  private loadInitialCategories(): Observable<Category[]> | void {
+    if (this.categoriesService.categories.length) return;
+
+    this.__categories$ = this.categoriesService.getCategories(0, 15).pipe(
+      tap((c) => console.log('carregou as categorias:', c)),
+      mergeMap(() => this.categoriesService.categories$)
     );
   }
 }
